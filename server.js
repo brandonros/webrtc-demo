206d458fa91bd2a54bb63d65379047aaf1210ac7
Brandon Ros <brandonros1@gmail.com>
2021-02-16 14:15:18 -0500

    /api

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ app.use(express.json())
 // static
 app.get('/', express.static('./'))
 // insert
-app.put('/state', (req, res) => {
+app.put('/api/state', (req, res) => {
   state[req.body.key] = req.body.value
   res.status(204)
   res.end()
 })
 // get
-app.get('/state', (req, res) => res.send(state))
+app.get('/api/state/:key', (req, res) => res.send(state[req.params.key]))
 // bind + listen
 app.listen(3000, () => console.log('Listening...'))
